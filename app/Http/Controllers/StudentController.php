1d8853f374f7a080e1383f296531dd45eb4c6a9a
elsaaputri <144228123+elsaaputri@users.noreply.github.com>
2025-05-10 22:26:20 +0700

    Part2

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use App\Models\Student;
+use Illuminate\View\view;
+
+class StudentController extends Controller
+{
+    //
+
+    public function index(): View
+    {
+
+
+    }
+
+
+    public function create()
+    {
+
+
+    }
+
+
+    public function store(request $request)
+    {
+        
+    }
+}
